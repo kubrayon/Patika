4c85cccd642d626e3bbde66e42d1154d5e17f5d6
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-09-29 19:39:22 +0200

    Add files via upload

---
+++ Switch-case/Switch-case/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Switch_case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Switch case yapısı, çok durumlu dallanma ifadelerinde if-else blokları yerine tercihen kullanılırlar. Switch-case ile yapılabilecek tüm işlemler if-else merdiveni ile de yapılabilmektedir. 
+
+            int gun = (int)DateTime.Now.DayOfWeek;
+            switch (gun)
+            {
+                case 1:
+                    Console.Write("Bugün Pazartesi");
+                    break;
+                case 2:
+                    Console.Write("Bugün Salı");
+                    break;
+                case 3:
+                    Console.Write("Bugün Çarşamba");
+                    break;
+                case 4:
+                    Console.Write("Bugün Perşembe");
+                    break;
+                case 5:
+                    Console.Write("Bugün Cuma");
+                    break;
+                case 6:
+                    Console.Write("Bugün Cumartesi");
+                    break;
+                case 7:
+                    Console.Write("Bugün Pazar");
+                    break;
+            }
+        }
+    }
+}
