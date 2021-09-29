da469e7c4fbac85ab480fb0e5632f963e4a4a9ab
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-09-29 19:26:19 +0200

    Add files via upload

---
+++ If-else/If-else/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace If_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //İf -Else karar yapıları belirli bir şarta bağlı olarak yerine getirilmesi istenen komutların çalıştırılması için kullanılmaktadır.
+            //Bu şart kimi zaman iki farklı ihtimal olabileceği gibi, üç yada daha fazla koşulda olabilmektedir.
+
+            int number;
+            Console.Write("Lütfen bir sayı girin:");
+            number = Convert.ToInt32(Console.ReadLine());
+
+            if (number > 0)
+
+                Console.WriteLine("{0} sayısı pozitif bir sayıdır.", number);
+
+            else if (number < 0)
+
+                Console.WriteLine("{0} sayısı negatif bir sayıdır.", number);
+
+            else
+
+                Console.WriteLine("Girilen sayı 0'a eşittir.");
+
+            Console.ReadKey();
+        }
+    }
+}
