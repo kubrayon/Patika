6853bbef1823aa102a4a51db07e24eb59d6a415e
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-09-30 20:52:31 +0300

    Add files via upload

---
+++ Recursive/Recursive/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Recursive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Recursive (özyinelemeli) fonksiyon, bir fonksiyonun kendini çağırması olarak ifade edilebilir. Diğer bir deyişle kendi kendini çağıran fonksiyonlarda denilebilir.
+
+            Console.Write("Bir sayı giriniz : ");
+
+            int input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(input + "! faktöriyel : " + Faktoriyel(input));
+
+            Console.ReadKey();
+            static int Faktoriyel(int sayi)
+            {
+                if (sayi <= 1)
+                {
+                    return 1;
+                }
+                return sayi * Faktoriyel(sayi - 1);
+            }
+        }
+    }
+}
