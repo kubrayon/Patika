57b3aa2e1e6952be7cfe25e430b67f39b03cdf79
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-09-30 22:18:09 +0300

    Add files via upload

---
+++ constructors/constructors/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace constructors
+{
+
+
+    //Class Constructors denilen kurucu metotlar bir  sınıf veya yapı oluşturulduğunda, kurucusu çağrılır
+    //Kurucular, sınıf veya yapı ile aynı ada sahiptir. Diğer metotlardan bazı farklılıkları vardır.
+    //Kurucu metod sınıf ismi ile aynı olmalıdır
+    //Public olarak tanımlanmalıdır.
+    //Geriye değer döndürmez.
+    class Urun
+    {
+        public string ad;
+        public double fiyat;
+
+        public Urun()   // metodumuzu oluşturduk
+        {
+            ad = "";
+            fiyat = 0;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun u = new Urun();
+
+            Console.WriteLine(u.ad);
+            Console.WriteLine(u.fiyat);
+        }
+    }
+}
