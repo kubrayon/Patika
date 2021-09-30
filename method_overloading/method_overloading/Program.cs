25b95d9059a06f923f8136d9610ede8b68bea3fd
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-09-30 22:07:07 +0300

    Add files via upload

---
+++ method_overloading/method_overloading/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace method_overloading
+{
+    class Program
+    {
+        static int ort(int n1, int n2)
+        {
+            int sonuc = (n1 + n2) / 2;
+            return sonuc;
+        }
+
+        static int ort(int n1, int n2, int n3)
+        {
+            int sonuc = (n1 + n2 + n3) / 2;
+            return sonuc;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ort(13, 5));
+            Console.WriteLine(ort(13, 8, 7));
+            Console.ReadKey();
+        }
+    }
+}
