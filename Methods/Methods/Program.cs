27a205d25b1186a45bcebbbfcc2dd44aea4e3382
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-09-30 21:56:49 +0300

    Add files via upload

---
+++ Methods/Methods/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Metodlar belirli işlemleri yerine getiren kod bloklarıdır.
+            //Aynı kodların tekrar tekrar kullanılması gereken durumlarda kolaylık sağlar
+
+            //Kullanımı;
+
+            Console.WriteLine("<Erişim biçimi> <Geri dönüş tipi> Metot_ismi(parametre)");
+
+            //Klavyeden girilen iki sayıdan hangiisnin büyük olduğunu geriye değer döndüren metot kullanarak yapalım
+
+            Console.WriteLine("Birinci sayıyı giriniz");
+            int n1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("İkinci sayıyı giriniz");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            int buyuk = kucukbuyuk(n1, n2);
+            Console.WriteLine("Girilen büüyk sayı=" + buyuk);
+            Console.ReadKey();
+
+        }
+        static int kucukbuyuk(int a, int b) //metodu burada tanımladık
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+
+    }
+}
