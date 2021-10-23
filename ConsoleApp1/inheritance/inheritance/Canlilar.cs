dfb4201aa6fee5c578437ee21fd93039593fe186
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-10-23 20:22:32 +0300

    Add files via upload
    added

---
+++ ConsoleApp1/inheritance/inheritance/Canlilar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inheritance
+{
+   public class Canlilar
+    {
+        public void Solunum()
+        {
+            Console.WriteLine("Canlılar solunum yaparlar.");
+        }
+        public void Beslenme()
+        {
+            Console.WriteLine("Canlılar beslenirler.");
+
+        }
+        public void Bosaltim()
+        {
+            Console.WriteLine("Canlılar boşaltım yaparlar.");
+        }
+    }
+}
