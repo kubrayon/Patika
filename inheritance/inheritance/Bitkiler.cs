51a692fbe72a66bb4106e5b974234d5187e83891
Kübra Yön <89014002+kubrayon@users.noreply.github.com>
2021-10-27 21:06:48 +0300

    Add files via upload

---
+++ inheritance/inheritance/Bitkiler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace inheritance
+{
+    public class Bitkiler : Canlilar
+    {
+        public void Fotosentez()
+        {
+            Console.WriteLine("Bitkiler fotosentez yaparlar.");
+
+        }
+        public class Tohumlu : Bitkiler
+        {
+            public void TohumlaCogalma()
+            {
+                Console.WriteLine("Tohumlu bitkiler tohumla çoğalırlar");
+            }
+            public class Tohumsuz : Bitkiler
+            {
+                public void SporlaCogalma()
+                {
+                    Console.WriteLine("Tohumsuz bitkiler sporla çoğalırlar.");
+                }
+            }
+        }
+    }
+}
